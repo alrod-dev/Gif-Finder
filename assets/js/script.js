a10997c57d7696a23e06f89bdca05ce536b29ea6
Alfredo Rodriguez <alrod909@gmail.com>
2017-08-10 13:21:47 -0600

    Fixed Issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
 
             console.log(person);
 
-            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
                 person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
             $.ajax({
